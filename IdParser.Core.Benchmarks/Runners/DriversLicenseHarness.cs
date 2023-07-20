3002579733435e615d73894e05c5eab443ed3661
Jon Sagara <jon@sagarasoftware.com>
2023-07-20 10:47:20 -0700

    Added a baseline benchmark for the instance based parser. It allocates 32k of memory for each invocation.

---
+++ IdParser.Core.Benchmarks/Runners/DriversLicenseHarness.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace IdParser.Core.Benchmarks.Runners;
+
+[SimpleJob(RuntimeMoniker.Net60)]
+[MemoryDiagnoser]
+public class DriversLicenseHarness
+{
+    private readonly string _licenseText;
+
+    public DriversLicenseHarness()
+    {
+        Console.WriteLine("Reading File...");
+        _licenseText = File.ReadAllText(Path.Combine(@"data\Licenses\CA.txt"));
+        Console.WriteLine("Done Reading File.");
+    }
+
+    [Benchmark(Baseline = true)]
+    public void Instance_ParseLicense()
+        => Barcode.Parse(_licenseText, Validation.None);
+}
