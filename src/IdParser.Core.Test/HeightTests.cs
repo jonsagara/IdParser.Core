05ad4d18f7d6f4fe46cb0df8dbb320714ccee0eb
Jon Sagara <jon@sagarasoftware.com>
2023-09-07 20:50:47 -0700

    Added height unit tests.

--- src/IdParser.Core.Test/HeightTests.cs
+++ src/IdParser.Core.Test/HeightTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using IdParser.Core.Test.Extensions;
+using Xunit;
 
 namespace IdParser.Core.Test;
 
@@ -13,42 +14,40 @@ public class HeightTests
         Assert.Equal(left, right);
     }
 
-    // I don't see the value in making Height implement IComparable.
-    //[Fact]
-    //public void ComparableTest()
-    //{
-    //    var first = new Height(feet: 6, inches: 2);
-    //    var second = new Height(feet: 5, inches: 8);
 
-    //    Assert.True(first.CompareTo(second) > 0);
-    //    Assert.True(second.CompareTo(first) < 0);
-    //}
+    [Fact]
+    public void FeetInchesToTotalInchesTest()
+    {
+        var height = new Height(feet: 5, inches: 9);
 
-    // Height no longer has a custom ToString() overload.
-    //[Fact]
-    //public void ImperialDisplayTest()
-    //{
-    //    var height = new Height(inches: 67);
-    //    var actual = height.ToString();
+        Assert.Equal(69, height.TotalInches);
+    }
 
-    //    Assert.Equal("5'7\"", actual);
-    //}
+    [Fact]
+    public void TotalInchesToFeetInchesTest()
+    {
+        var height = new Height(totalInches: 69);
 
-    //[Fact]
-    //public void MetricDisplayTest()
-    //{
-    //    var height = new Height(centimeters: 175);
-    //    var actual = height.ToString();
+        Assert.Equal(5, height.FeetAndInches.Feet);
+        Assert.Equal(9, height.FeetAndInches.Inches);
+    }
 
-    //    Assert.Equal("175 cm", actual);
-    //}
+    [Fact]
+    public void FeetInchesToCentimetersTest()
+    {
+        var height = new Height(totalInches: 69);
 
-    //[Fact]
-    //public void RoundingTest()
-    //{
-    //    var height = new Height(inches: 62);
-    //    var actual = height.ToString();
+        var expectedCm = 175.3;
 
-    //    Assert.Equal("5'2\"", actual);
-    //}
+        Assert.True(height.Centimeters.Equals1DigitPrecision(expectedCm));
+    }
+
+    [Fact]
+    public void TotalInchesToFeetInches_ThreeFeet()
+    {
+        var height = new Height(totalInches: 36);
+
+        Assert.Equal(3, height.FeetAndInches.Feet);
+        Assert.Equal(0, height.FeetAndInches.Inches);
+    }
 }
