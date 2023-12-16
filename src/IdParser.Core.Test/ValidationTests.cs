b8a85ffb9c79fbb1cbfc2382b06adbd638c37066
Jon Sagara <jon@sagarasoftware.com>
2023-12-16 12:13:18 -0800

    #17: Converting ValidationTests.

--- src/IdParser.Core.Test/ValidationTests.cs
+++ src/IdParser.Core.Test/ValidationTests.cs
@@ -7,12 +7,12 @@ public class ValidationTests
     [Fact]
     public void InvalidLengthTest()
     {
-        Assert.Throws<ArgumentException>(() => Barcode.Parse("ABC123"));
+        Assert.Throws<ArgumentException>(() => Barcode.Parse2("ABC123"));
     }
 
     [Fact]
     public void InvalidComplianceIndicatorTest()
     {
-        Assert.Throws<ArgumentException>(() => Barcode.Parse(new string('A', 32)));
+        Assert.Throws<ArgumentException>(() => Barcode.Parse2(new string('A', 32)));
     }
 }
